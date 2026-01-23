af7a70b50060c7a635e27bd8ab73d98efda52f48
rfclz <rafafadilaras@gmail.com>
2026-01-23 18:02:24 +0700

    Kompilasi Typescript

---
+++ dist/say-hello.d.ts
@@ -0,0 +1,2 @@
+export declare function sayHello(name: string): string;
+//# sourceMappingURL=say-hello.d.ts.map
---
+++ src/say-hello.js
@@ -0,0 +1,4 @@
+export function sayHello(name) {
+    return `Hello ${name}`;
+}
+//# sourceMappingURL=say-hello.js.map
