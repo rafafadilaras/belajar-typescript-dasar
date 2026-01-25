824a31323031c521b2a57374f8925d7502a959a7
rfclz <rafafadilaras@gmail.com>
2026-01-25 20:28:06 +0700

    Type Alias

---
+++ src/type-alias.ts
@@ -0,0 +1,13 @@
+export type ID = string | number;
+
+export type Category = {
+    id: ID;
+    name: string;
+}
+
+export type Product = {
+    id: string;
+    name: string;
+    price: number;
+    category: Category;
+}
