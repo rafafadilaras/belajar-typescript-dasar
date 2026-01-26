8b8fb55d02bc125d7706f9965ae5ff832e17f4c8
rfclz <rafafadilaras@gmail.com>
2026-01-26 19:29:50 +0700

    Function di Interface

--- test/interface.test.ts
+++ test/interface.test.ts
@@ -77,6 +77,23 @@ describe('Interface', function() {
         };
 
         console.info(manager);
-        
-    })
+
+    });
+
+    it('should support function in interface', function() {
+        interface Person {
+            name: string;
+            sayHello: (name: string) => string;
+        }
+
+        const person: Person = {
+            name: "Rafa",
+            sayHello: function(name: string): string {
+                return `Hello ${name}, my name is ${this.name}`;
+            }
+        }
+
+        console.info(person.sayHello("Rafa"));
+
+    });
 });
