97bddad63ec77087a9911a80850a50b79d7e6cf7
rfclz <rafafadilaras@gmail.com>
2026-01-26 19:54:29 +0700

    Function Parameter

--- test/function.test.ts
+++ test/function.test.ts
@@ -1,5 +1,5 @@
-describe('Function', function() {
-    it('should support in typescript', function() {
+describe('Function', function () {
+    it('should support in typescript', function () {
 
         function sayHello(name: string): string {
             return `Hello ${name}`;
@@ -13,5 +13,44 @@ describe('Function', function() {
 
         printHello("Rafa");
 
+    });
+
+    it('should support default type', function () {
+        function sayHello(name: string = "Guest"): string {
+            return `Hello ${name}`;
+        }
+
+        expect(sayHello()).toBe("Hello Guest");
+        expect(sayHello("Rafa")).toBe("Hello Rafa");
+
+    });
+
+    it('should support rest parameter', function() {
+        function sum(...values: number[]): number {
+            let total = 0;
+            for (const value of values) {
+                total += value;
+            }
+            return total;
+        }
+
+        expect(sum(1,2,3,4,5)).toBe(15);
+
+    });
+
+    it('should support optional parameter', function() {
+        function sayHello(firstName : string, lastName? : string): string {
+
+            if(lastName) {
+                return `Hello ${firstName} ${lastName}`;
+            }else {
+                return `Hello ${firstName}`;
+            }
+            
+        }
+
+        expect(sayHello("Rafa")).toBe("Hello Rafa");
+        expect(sayHello("Rafa", "Fadil")).toBe("Hello Rafa Fadil");
+        
     });
 });
