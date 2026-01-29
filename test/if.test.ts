e7a2d4c44ba1ef1b8e85cb1d42ba2766b8ac7d7e
rfclz <rafafadilaras@gmail.com>
2026-01-29 13:50:11 +0700

    If Statement

---
+++ test/if.test.ts
@@ -0,0 +1,15 @@
+describe('If statement', function() {
+    it('should support in typescript', function() {
+
+        const examValue = 90;
+        
+        if(examValue > 80) {
+            console.info("Good");
+        }else if (examValue > 60) {
+            console.info("Not Bad");
+        }else {
+            console.info("Try Again");
+        }
+
+    });
+});
