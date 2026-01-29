3d8c9f3cd3350a72b38f852e9e09735e321ae1f3
rfclz <rafafadilaras@gmail.com>
2026-01-29 14:00:14 +0700

    For Loop

---
+++ test/loop.test.ts
@@ -0,0 +1,18 @@
+describe('Loop', function() {
+    it('should support for loop', function() {
+
+        const names : string[] = ["Rafa", "Fadil", "Aras"];
+
+        for (let i = 0; i < names.length; i++) {
+            console.info(names[i]);
+        }
+
+        for (const name of names) {
+            console.info(name);
+        }
+
+        for (const index in names) {
+            console.info(names[index]);
+        }
+    })
+});
