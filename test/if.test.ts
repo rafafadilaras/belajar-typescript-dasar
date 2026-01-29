2113c364acd7cab38fc864441948ae5a4cd5bdc3
rfclz <rafafadilaras@gmail.com>
2026-01-29 13:51:48 +0700

    Ternary Operator

--- test/if.test.ts
+++ test/if.test.ts
@@ -12,4 +12,12 @@ describe('If statement', function() {
         }
 
     });
+
+    it('should support ternary operator', function() {
+
+        const value = 80;
+        const say = value >= 75 ? "Congratulation" : "Try Again";
+        console.info(say);
+
+    });
 });
